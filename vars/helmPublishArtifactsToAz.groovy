ea9c083c91d4cfd7e2f4863a75a7d7d8c6db4193
Endre Karlson <endre.karslon@gmail.com>
2018-09-14 15:10:04 +0200

    Add helper to publish helm artifacts

---
+++ vars/helmPublishArtifactsToAz.groovy
@@ -0,0 +1,24 @@
+/* This is a utility function to upload
+  - helm chart packaged via helmPackage or alike
+  - helm value files residing under values/file.yaml, the files will be changed to have names like <original filaneme>-<chartVersion>.yaml
+*/
+def call(appName, Map opts = [:]) {
+  def chartDef = readYaml file: "charts/${appName}/Chart.yaml"
+  if (!opts.version) {
+    chartVersion = chartDef.version
+  } else {
+    chartVersion = opts.version
+  }
+
+  azureUpload storageCredentialId: 'az-artifacts', storageType: 'blobstorage', containerName: appName, filesPath: chartArtifact, virtualPath: "packages"
+  def valueFiles = findFiles(glob: 'values/**.yaml')
+  valueFiles.each {f ->
+    def base = f.name.minus(".yaml")
+
+    /* This copy thing is just a way to get around azureUpload not allowing you to set the remote path correctly */
+    def releaseValueFileName = "${base}-${chartVersion}.yaml"
+    sh "cp values/${f.name} values/$releaseValueFileName"
+
+    azureUpload storageCredentialId: 'az-artifacts', storageType: 'blobstorage', containerName: appName, filesPath: "values/${releaseValueFileName}"
+  }
+}
