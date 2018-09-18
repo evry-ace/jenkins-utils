0856508b3169ff404b369e43b44ee0dceae226fc
Endre Karlson <endre.karslon@gmail.com>
2018-09-18 17:26:28 +0200

    Copy values to temp dir

--- vars/helmPublishArtifactsToAz.groovy
+++ vars/helmPublishArtifactsToAz.groovy
@@ -3,7 +3,7 @@
   - helm value files residing under values/file.yaml, the files will be changed to have names like <original filaneme>-<chartVersion>.yaml
 */
 def call(appName, Map opts = [:]) {
-  def chartDef = readYaml file: "charts/${appName}/Chart.yaml"
+  def chartDef = readYaml file: "chart/${appName}/Chart.yaml"
   if (!opts.version) {
     chartVersion = chartDef.version
   } else {
@@ -11,14 +11,24 @@ def call(appName, Map opts = [:]) {
   }
 
   azureUpload storageCredentialId: 'az-artifacts', storageType: 'blobstorage', containerName: appName, filesPath: "${appName}-${chartVersion}.tgz", virtualPath: "packages"
+
   def valueFiles = findFiles(glob: 'values/**.yaml')
   valueFiles.each {f ->
     def base = f.name.minus(".yaml")
 
     /* This copy thing is just a way to get around azureUpload not allowing you to set the remote path correctly */
     def releaseValueFileName = "${base}-${chartVersion}.yaml"
-    sh "cp values/${f.name} values/$releaseValueFileName"
+    sh """
+    mv values values.tmp
+    cp -R values.tmp values
+    mv values/${f.name} values/$releaseValueFileName
+    """
 
     azureUpload storageCredentialId: 'az-artifacts', storageType: 'blobstorage', containerName: appName, filesPath: "values/${releaseValueFileName}"
+
+    sh """
+    rm -rf values
+    mv values.tmp values
+    """
   }
 }
