5a4c91e1fefdbba4b89c2d9f79d8559150a67cf0
Endre Karlson <endre.karslon@gmail.com>
2018-09-24 21:17:18 +0200

    Need to set chart_pkg

--- vars/spinnakerParamsToProps.groovy
+++ vars/spinnakerParamsToProps.groovy
@@ -11,6 +11,9 @@ def call(Map params, List<Map> artifacts) {
       def key = artifact.name.split("/").last().replace(".yaml", "")
       data.push("values_${key}=${artifact.reference}")
     }
+    if (artifact.name =~ /\S+?\d+\.\d+\.\d+(|\S+)\.tgz/) {
+      data.push("chart_pkg=${artifact.reference}")
+    }
   }
 
   return data
