d57966caaa36a87664634a7e40c9871aba5bfb0f
Endre Karlson <endre.karslon@gmail.com>
2018-09-28 23:58:41 +0200

    Make it possible to pass topic

--- vars/spinPublishBuildToGcpPubsub.groovy
+++ vars/spinPublishBuildToGcpPubsub.groovy
@@ -7,12 +7,14 @@
 */
 def call(Map opts = [:]) {
   def secret = opts.secret ?: "gcp-jenkins-pubsub"
+  def topic = opts.topic ?: "build_done"
+
   withCredentials([[$class: 'StringBinding', credentialsId: secret, variable: 'GCP_KEY']]) {
     docker.image('google/cloud-sdk').inside("-e GCP_KEY=${env.GCP_KEY} -e GCP_KEY_FILE=/tmp/google-key.json -u 0:0 -v ${pwd()}:/code -w /code") {
       sh """
       echo $GCP_KEY | base64 -d > /tmp/google-key.json
       gcloud auth activate-service-account --key-file=/tmp/google-key.json --project ${opts.project}
-      gcloud pubsub topics publish build_done --message "`cat build.json`"
+      gcloud pubsub topics publish ${topic} --message "`cat build.json`"
       """
     }
   }
